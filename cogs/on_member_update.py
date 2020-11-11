7fee6248af4948098ea7f8f822401d5259f5cdc5
Sonic4999 <sonictbm@gmail.com>
2020-11-11 15:43:37 -0500

    Another fix to on_member_update

--- cogs/on_member_update.py
+++ cogs/on_member_update.py
@@ -10,14 +10,18 @@ class MemberUpdate(commands.Cog):
         if self.bot.sonic_bot_role in before.roles:
             status_chan = self.bot.get_channel(776114750064951296)
 
-            if before.raw_status != after.raw_status and after.raw_status == "offline":
-                await status_chan.send(f"{after.mention} is offline. Please wait - this tends to happen semi-frequently" +
-                ", and the bot will come back up soon automatically.")
+            if before.activities != after.activities:
+                # yes, this is important to check - if a bot goes offline, it loses its activities
+                # and when a bot gets back online, it gets back its activities
 
-            elif before.activities != after.activities and after.activities != ():
-                # this more or less checks if a bot actually has an activity, which bots like
-                # Seraphim will have only when they are done
-                await status_chan.send(f"{after.mention} is back online.")
+                if before.raw_status != after.raw_status and after.raw_status == "offline":
+                    await status_chan.send(f"{after.mention} is offline. Please wait - this tends to happen semi-frequently" +
+                    ", and the bot will come back up soon automatically.")
+
+                elif after.raw_status == "online" and after.activities != ():
+                    # this more or less checks if a bot actually has an activity, which bots like
+                    # Seraphim will have only when they are done
+                    await status_chan.send(f"{after.mention} is back online.")
 
 def setup(bot):
     bot.add_cog(MemberUpdate(bot))
