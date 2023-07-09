9f35733eef9934f86d0704ff04f531d08c32e494
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-07-09 18:35:42 -0400

    feat: add discords.com support

--- exts/vote_handling.py
+++ exts/vote_handling.py
@@ -30,6 +30,7 @@ class VoteHandling(ipy.Extension):
         app = web.Application()
         app.add_routes([web.post("/topgg", self.topgg_handling)])
         app.add_routes([web.post("/dbl_rpl", self.dbl_handling)])
+        app.add_routes([web.post("/discordscom", self.discords_com_handler)])
         self.runner = web.AppRunner(app)
         await self.runner.setup()
         site = web.TCPSite(self.runner, "127.0.0.1", 8000)
@@ -66,6 +67,21 @@ class VoteHandling(ipy.Extension):
             725483868777611275,
             "Discord Bot List",
             "https://discordbotlist.com/bots/realms-playerlist-bot",
+        )
+
+    async def discords_com_handler(self, request: web.Request):
+        authorization = request.headers.get("Authorization")
+        if not authorization or authorization != os.environ["DISCORDSCOM_AUTH"]:
+            return web.Response(status=401)
+
+        vote_data = await request.json(loads=orjson.loads)
+        user_id = int(vote_data["user"])
+        return await self.handle_vote(
+            f"<@{user_id}>",
+            user_id,
+            int(vote_data["bot"]),
+            "Discords.com",
+            "https://discords.com/bots/bot/{bot_id}}",
         )
 
     async def handle_vote(
